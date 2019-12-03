af36d39f83a5233fffb15e24bd4da11ea425a948
Fanome <fabionogueiram@yahoo.com.br>
2019-12-03 12:01:29 -0300

    Começando a colocar as pecas do tabuleiro

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,27 +8,12 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            //PosicaoXadrez pos = new PosicaoXadrez('c', 7);
-
-            //Console.WriteLine(pos);
-
-            //Console.WriteLine(pos.toPosicao());
 
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
-
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
-                //tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(1, 3));
-                //tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(0, 2));
-
-                Tela.imprimeTabuleiro(tab);
-
-
+                Tela.imprimeTabuleiro(partida.tab);
             }
             catch (TabuleiroException e)
             {
